ca04ada082cb16652188d5a51e6851bc8ab9e59f
sionxlee <32076778+sionxlee@users.noreply.github.com>
2017-09-18 21:35:21 -0700

    1
    1

---
+++ Assets/Scripts/SlashScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SlashScript : MonoBehaviour {
+
+
+    private int count;
+    public Text countText;
+    public Text winText;
+
+    // Use this for initialization
+    void Start ()
+    {
+        count = 0;
+        SetCountText();
+        winText.text = "";
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
